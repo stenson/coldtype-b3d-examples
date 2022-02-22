095ae5d27bd62ca2e8c3db7825a12668f10679da
Rob Stenson <rob.stenson@gmail.com>
2022-02-21 17:18:57 -0800

    add a blend file for simple

--- simple.py
+++ simple.py
@@ -1,11 +1,11 @@
 from coldtype import *
 from coldtype.blender import *
 
-@b3d_animation()
+@b3d_animation(timeline=60)
 def simple(f):
-    return (StSt("E", Font.ColdtypeObviously(), 500
+    return (StSt("CT", Font.ColdtypeObviously(), 500
         , wdth=f.e("eeio"))
         .align(f.a.r)
         .pen()
         .ch(b3d(lambda bp: bp
-            .extrude(0.5))))
+            .extrude(f.e("eeio", r=(0.25, 5))))))
